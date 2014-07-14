4a692ea0cc6e50989e762c594624ec7d0de8ef61
Administrator <connorknabe@gmail.com>
2014-07-13 19:37:34 -0500

    Added some code for drawing the view

--- TouchTracker/CKDrawView.m
+++ TouchTracker/CKDrawView.m
@@ -7,7 +7,46 @@
 //
 
 #import "CKDrawView.h"
+#import "CKLine.h"
+
+@interface CKDrawView()
+
+@property (nonatomic, strong) CKLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
 
 @implementation CKDrawView
+
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if(self) {
+        self.finishedLines = [[NSMutableArray alloc]init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
+}
+
+
+-(void)strokeLine:(CKLine *)line{
+    
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+    
+}
+
+-(void) drawRect:(CGRect)rect{
+    [[UIColor blackColor]set];
+    for (CKLine *line in self.finishedLines) 
+        [self strokeLine:line];
+    }
+}
+
 
 @end
