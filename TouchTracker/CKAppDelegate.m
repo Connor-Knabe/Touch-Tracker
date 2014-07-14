6271e432e52c18077da9ba46511e2ca766a00ecc
Administrator <connorknabe@gmail.com>
2014-07-13 19:31:12 -0500

    Added a root view controller

--- TouchTracker/CKAppDelegate.m
+++ TouchTracker/CKAppDelegate.m
@@ -7,13 +7,18 @@
 //
 
 #import "CKAppDelegate.h"
-
+#import "CKDrawViewController.h"
 @implementation CKAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
+    
+    CKDrawViewController *dvc = [[CKDrawViewController alloc]init];
+    self.window.rootViewController = dvc;
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
