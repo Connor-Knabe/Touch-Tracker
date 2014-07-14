ef223893ef6802d615aad6f282527f657174354d
Administrator <Connor@Connors-MBP.local>
2014-07-13 19:16:33 -0500

    first commit

---
+++ TouchTracker/CKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CKAppDelegate.h
+//  TouchTracker
+//
+//  Created by Administrator on 7/13/14.
+//  Copyright (c) 2014 Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Administrator on 7/13/14.
+//  Copyright (c) 2014 Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CKAppDelegate class]));
+    }
+}
